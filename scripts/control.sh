9eca5d61f50d6e6e90cb008653a4489a8e3e9bf5
jakelooker <jakelooker@gmail.com>
2025-11-08 11:31:24 +0000

    Add control script

---
+++ scripts/control.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [ "$1" == "drain" ]; then
+    NODES=($(kubectl get nodes -o name))
+    for NODE in "${NODES[@]}"; do
+        kubectl drain --ignore-daemonsets --delete-emptydir-data $NODE
+    done
+    echo ""
+    echo "Draining completed"
+    kubectl get node
+fi
+
+if [ "$1" == "start" ]; then
+    NODES=($(kubectl get nodes -o name))
+    for NODE in "${NODES[@]}"; do
+        kubectl uncordon $NODE
+    done
+fi
+
+echo "Help"
+echo "Arguments: drain, start"
+echo "./control.sh drain"
+echo "Get all nodes and drain them one by one"
+echo "./control.sh start"
+echo "Uncordon all nodes"
